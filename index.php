81a7bf1f110f529cadb1ccf7c3be8b18f18220c1
s.konovalov <s.konovalov@intensa.ru>
2021-09-17 12:06:33 +0300

    bot command add

--- index.php
+++ index.php
@@ -23,22 +23,6 @@ $bot->command('help', function ($message) use ($bot) {
     /help - помощь';
     $bot->sendMessage($message->getChat()->getId(), $answer);
 });
-$bot->command('fun', function ($message) use ($bot) {
-    $row = PoemModel::getItem(1);
-    $pic = $row->image;
-    $bot->sendPhoto($message->getChat()->getId(), $pic);
-});
-$bot->command('getpic', function ($message) use ($bot) {
-    $answer = 'Изображение:';
-    $pic = "https://icopydoc.ru/wp-content/uploads/fortelegrambot.jpg";
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-    $bot->sendPhoto($message->getChat()->getId(), $pic);
-});
-$bot->command('bestcar', function ($message) use ($bot) {
-    $answer = 'Зеленый матиз!:';
-    $pic = "https://a.d-cd.net/4a1f644s-960.jpg";
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-    $bot->sendPhoto($message->getChat()->getId(), $pic);
-});
+
 // запускаем обработку
 $bot->run();
