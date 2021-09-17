957cc454b713a49b05a59cef16294072489eb288
s.konovalov <s.konovalov@intensa.ru>
2021-09-17 11:21:48 +0300

    bot start

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 echo 'he';
 // подключаемся к API
 require_once("vendor/autoload.php");
-//use App\models\PoemModel as PoemModel;
+use App\models\PoemModel as PoemModel;
 // создаем переменную бота
 $token = "1980530629:AAGp6UbT3J_tJrLDfvbwrw7CaAp3w21cXYk";
 $bot = new \TelegramBot\Api\Client($token);
@@ -18,6 +18,13 @@ $bot->command('help', function ($message) use ($bot) {
     $answer = 'Команды:
 /help - помощь';
     $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->command('getpic', function ($message) use ($bot) {
+    $answer = 'Изображение:';
+    $pic = "https://icopydoc.ru/wp-content/uploads/fortelegrambot.jpg";
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+    $bot->sendPhoto($message->getChat()->getId(), $pic);
 });
 // запускаем обработку
 $bot->run();
