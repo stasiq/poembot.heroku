4626daa5079dd47b7cbfc1ab5ad87d50e0e7e988
s.konovalov <s.konovalov@intensa.ru>
2021-09-17 16:13:09 +0300

    bot command add

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ require_once("vendor/autoload.php");
 use Faker\Factory as Faker;
 use App\models\PoemModel as PoemModel;
 $row =  PoemModel::getItem('sad');
-echo $row[0];
+echo $row[0] . $row[1];
 $faker = Faker::create();
 ?>
 <!--<img src="--><?//=$faker->imageUrl(640, 480, 'animals')?><!--" alt="">-->
