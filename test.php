f4dd191b7b60abbb5cd6fe40d7774835c65ab041
s.konovalov <s.konovalov@intensa.ru>
2021-09-17 15:18:52 +0300

    bot command add

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
 require_once("vendor/autoload.php");
 use Faker\Factory as Faker;
-$faker = Faker::create('ru_RU');
-echo $faker->imageUrl(640, 480, 'animals', true);
+$faker = Faker::create();
+?>
+<img src="<?=$faker->imageUrl(640, 480, 'animals')?>" alt="">
