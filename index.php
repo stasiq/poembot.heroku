6eade175b34cd3628fc611c44b43b4d16f5f3e8c
s.konovalov <s.konovalov@intensa.ru>
2021-09-17 11:07:06 +0300

    bot start

--- index.php
+++ index.php
@@ -3,18 +3,39 @@ header('Content-Type: text/html; charset=utf-8');
 
 // подключаемся к API
 require_once("vendor/autoload.php");
-use \TelegramBot\Api\Client as TelegramBot;
+use App\models\PoemModel as PoemModel;
 // создаем переменную бота
 $token = "1980530629:AAGp6UbT3J_tJrLDfvbwrw7CaAp3w21cXYk";
-$bot = new TelegramBot($token);
+$bot = new \TelegramBot\Api\Client($token);
 
-// если Телеграм-бот не зарегистрирован - регистрируем
-if(!file_exists("registered.trigger")){
+// обязательное. Запуск бота
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+// помощь
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - помощь';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+// запускаем обработку
+$bot->run();
 
-    $page_url = "https://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-    echo $page_url;
-    $result = $bot->setWebhook($page_url);
-    if($result){
-        file_put_contents("registered.trigger",time()); // создаем файл дабы остановить повторные регистрации
-    }
-}
+$rows = PoemModel::getList();
+$items = new poemController;
+$items->actionList();
+?>
+
+<div class="block">
+    <?php
+    foreach ($rows as $row) {
+        ?>
+        <div class="item">
+            <img src="<?=$row->image ?>" alt="">
+            <h3><?=$row->title?></h3>
+            <p><?=$row->text?></p>
+        </div>
+    <?php
+    }?>
+</div>
