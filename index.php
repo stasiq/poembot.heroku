14f5330355c580bb3ee846bfb983b5fa79b82e1c
s.konovalov <s.konovalov@intensa.ru>
2021-09-17 09:53:03 +0300

    heroku procfile

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
+echo 'Hello heroku!';
 //require_once __DIR__ . '/vendor/autoload.php';
 //
 //use Telegram\Bot\Api;
 //
 //$telegram = new Api('1980530629:AAGp6UbT3J_tJrLDfvbwrw7CaAp3w21cXYk'); //Устанавливаем токен, полученный у BotFather
 //$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
-echo 'Hello heroku!';
