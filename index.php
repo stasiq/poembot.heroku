910cd88a2304f558e99741d1a4323a0a2e8b14d1
s.konovalov <s.konovalov@intensa.ru>
2021-09-17 11:12:32 +0300

    bot start

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 echo 'he';
-//// подключаемся к API
-//require_once("vendor/autoload.php");
-////use App\models\PoemModel as PoemModel;
-//// создаем переменную бота
-//$token = "1980530629:AAGp6UbT3J_tJrLDfvbwrw7CaAp3w21cXYk";
-//$bot = new \TelegramBot\Api\Client($token);
-//
-//// обязательное. Запуск бота
-//$bot->command('start', function ($message) use ($bot) {
-//    $answer = 'Добро пожаловать!';
-//    $bot->sendMessage($message->getChat()->getId(), $answer);
-//});
-//// помощь
-//$bot->command('help', function ($message) use ($bot) {
-//    $answer = 'Команды:
-///help - помощь';
-//    $bot->sendMessage($message->getChat()->getId(), $answer);
-//});
-//// запускаем обработку
-//$bot->run();
+// подключаемся к API
+require_once("vendor/autoload.php");
+//use App\models\PoemModel as PoemModel;
+// создаем переменную бота
+$token = "1980530629:AAGp6UbT3J_tJrLDfvbwrw7CaAp3w21cXYk";
+$bot = new \TelegramBot\Api\Client($token);
+
+// обязательное. Запуск бота
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+// помощь
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - помощь';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+// запускаем обработку
+$bot->run();
