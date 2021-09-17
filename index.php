cfe04aae952d18b87fb6470a4697d50024cb3ee3
s.konovalov <s.konovalov@intensa.ru>
2021-09-17 10:36:03 +0300

    регистрация бота

--- index.php
+++ index.php
@@ -3,18 +3,14 @@ header('Content-Type: text/html; charset=utf-8');
 
 // подключаемся к API
 require_once("vendor/autoload.php");
+use \TelegramBot\Api\Client as TelegramBot;
 // создаем переменную бота
 $token = "1980530629:AAGp6UbT3J_tJrLDfvbwrw7CaAp3w21cXYk";
-$bot = new \TelegramBot\Api\Client($token);
+$bot = new TelegramBot($token);
 
 // если Телеграм-бот не зарегистрирован - регистрируем
 if(!file_exists("registered.trigger")){
-    /**
-     * файл registered.trigger будет создаваться после регистрации бота.
-     * если этого файла не существует, значит бот не
-     * зарегистрирован в Телеграмм
-     */
-// URl текущей страницы
+
     $page_url = "https://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
     echo $page_url;
     $result = $bot->setWebhook($page_url);
