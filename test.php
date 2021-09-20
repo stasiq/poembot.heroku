b7be5dc7a0f8beb4e9cefbdb51ee2ede3b8ed866
s.konovalov <s.konovalov@intensa.ru>
2021-09-20 12:32:00 +0300

    БД тест

--- test.php
+++ test.php
@@ -3,10 +3,12 @@ error_reporting(E_ALL);
 require_once("vendor/autoload.php");
 
 //use App\models\PoemModel as PoemModel;
+$db = parse_url(getenv("postgres://kkaegwqhgwgytp:9c5ff6d4472658b746f28825636db218d3e33094d060153fee0e7b3623eda7f2@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/db2ll6v2e9ukh3"));
+$db["path"] = ltrim($db["path"], "/");
+var_dump($db);
 
-
-$conn = pg_connect(getenv("postgres://kkaegwqhgwgytp:9c5ff6d4472658b746f28825636db218d3e33094d060153fee0e7b3623eda7f2@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/db2ll6v2e9ukh3"))
-or die('Не удалось соединиться: ' . pg_last_error());
+//$conn = pg_connect(getenv("postgres://kkaegwqhgwgytp:9c5ff6d4472658b746f28825636db218d3e33094d060153fee0e7b3623eda7f2@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/db2ll6v2e9ukh3"))
+//or die('Не удалось соединиться: ' . pg_last_error());
 //
 //// Выполнение SQL-запроса
 //
