9bf39479f07a86c486b81ceac9c558d2b66e5889
s.konovalov <s.konovalov@intensa.ru>
2021-09-20 10:23:47 +0300

    БД тест

--- app/classes/Db.php
+++ app/classes/Db.php
@@ -7,7 +7,7 @@ use PDO;
 
 class Db
 {
-    private static $db = null;
+    public static $db = parse_url(getenv('//postgres://kkaegwqhgwgytp:9c5ff6d4472658b746f28825636db218d3e33094d060153fee0e7b3623eda7f2@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/db2ll6v2e9ukh3')):
     private static $instance = null;
 
     static function getConnect()
--- test.php
+++ test.php
@@ -4,6 +4,6 @@ require_once("vendor/autoload.php");
 use Faker\Factory as Faker;
 use App\models\PoemModel as PoemModel;
 
-$row = PoemModel::getItem('sad');
-echo $row[0] . $row[1];
-$faker = Faker::create();
+$db = parse_url(getenv('//postgres://kkaegwqhgwgytp:9c5ff6d4472658b746f28825636db218d3e33094d060153fee0e7b3623eda7f2@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/db2ll6v2e9ukh3'));
+
+var_dump($db);
