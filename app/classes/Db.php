6e57237fd804a089100938f525ea2186e2cfed19
s.konovalov <s.konovalov@intensa.ru>
2021-09-20 12:19:51 +0300

    БД тест

--- app/classes/Db.php
+++ app/classes/Db.php
@@ -24,4 +24,8 @@ class Db
           ));
         return $pdo;
     }
+    static function getConnectPg() {
+        $conn = pg_connect(getenv("postgres://kkaegwqhgwgytp:9c5ff6d4472658b746f28825636db218d3e33094d060153fee0e7b3623eda7f2@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/db2ll6v2e9ukh3"));
+
+    }
 }
--- test.php
+++ test.php
@@ -1,10 +1,13 @@
 <?php
 require_once("vendor/autoload.php");
 
-use Faker\Factory as Faker;
 use App\models\PoemModel as PoemModel;
 
-$rows = PoemModel::getList();
-foreach ($rows as $row) {
-    var_dump($row);
-}
+
+$conn = pg_connect(getenv("postgres://kkaegwqhgwgytp:9c5ff6d4472658b746f28825636db218d3e33094d060153fee0e7b3623eda7f2@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/db2ll6v2e9ukh3"))
+or die('Не удалось соединиться: ' . pg_last_error());
+
+// Выполнение SQL-запроса
+
+$query = 'SELECT * FROM poems';
+$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
